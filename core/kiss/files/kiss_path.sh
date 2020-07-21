21ebd7f067493d16c91b6fe34b166fb48fa1421a
James Davies <james@jedavies.dev>
2020-07-21 20:25:10 +0000

    Add overrides, update KISS_PATH.

--- core/kiss/files/kiss_path.sh
+++ core/kiss/files/kiss_path.sh
@@ -1,1 +1,1 @@
-export KISS_PATH=/var/db/kiss/glasnost/core:/var/db/kiss/repo/core:/var/db/kiss/repo/extra:/var/db/kiss/repo/xorg:/var/db/kiss/community/community
+export KISS_PATH=/var/db/kiss/glasnost/core:/var/db/kiss/glasnost/overrides:/var/db/kiss/repo/core:/var/db/kiss/repo/extra:/var/db/kiss/repo/xorg:/var/db/kiss/community/community
